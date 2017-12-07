fafd13340261fcb4d17c96c230874e1892042e8a
Tess Craig <tessashleycraig89@gmail.com>
2017-12-07 17:27:48 -0600

    Commit 1 - Verbal questions

---
+++ solutions.js
@@ -0,0 +1,19 @@
+// Verbal Questions
+2. parameter is a type of variable that a function takes in and is how the input is represented in the fuction, while argument is an input that is provided when calling a function
+3.return statements specify the output of a function with the return statment and a return value of a function can be used to pass as data, while a console.log can not be used to pass as data.
+//Palindrome
+
+
+//Digit Sun
+
+
+//Pythagoras
+
+
+//Sum Array
+
+
+//Prime Numbers
+
+
+//Insert Dash
