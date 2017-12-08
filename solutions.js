fdd3ee1d2c30b7b2c74f80205ecdcc3d8530a4cf
Tess Craig <tessashleycraig89@gmail.com>
2017-12-07 18:08:44 -0600

    Commit 2 - Palindrome

--- solutions.js
+++ solutions.js
@@ -1,8 +1,16 @@
 // Verbal Questions
-2. parameter is a type of variable that a function takes in and is how the input is represented in the fuction, while argument is an input that is provided when calling a function
-3.return statements specify the output of a function with the return statment and a return value of a function can be used to pass as data, while a console.log can not be used to pass as data.
+// 2. parameter is a type of variable that a function takes in and is how the input is represented in the fuction, while argument is an input that is provided when calling a function
+// 3.return statements specify the output of a function with the return statment and a return value of a function can be used to pass as data, while a console.log can not be used to pass as data.
 //Palindrome
-
+// const checkPalindrome = (myString) => {
+//   const upperCase= myString.toUpperCase();
+//   const reverseString = upperCase.split('').reverse().join('');
+//   if (upperCase === reverseString) {
+//     console.log('true')
+//   } else {
+//       console.log('false');
+//   }
+// }
 
 //Digit Sun
 
